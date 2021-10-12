891021613f36fd4bb4feb7f62303a7dc0b95c2a6
DESKTOP-UL97IHH\admin <dbc200188@gmail.com>
2021-10-12 16:59:48 +0900

    only client

--- server/data/member_Schema.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const memberSchema = new mongoose.Schema({
-  member_name: String,
-  member_Fin_Interest: Array
-
-});
-
-module.exports = mongoose.model('member', memberSchema);
